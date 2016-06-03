c86998c884fc6e3796f40028d3bc6362f3637caf
Mallabaev Azamat <mallabaev.an@yandex.ru>
2016-06-03 14:43:14 +0400

    fix bug (Tehanu.Attributes.TotalAttribute was not System.Attribute)

--- tehanu.attributes.fs
+++ tehanu.attributes.fs
@@ -3,7 +3,8 @@ namespace Tehanu.Attributes
 open System
 
 [<AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)>]
-type EffectAttribute() = class end
+type EffectAttribute() =
+  inherit System.Attribute()
 
 type TotalAttribute(expr: string) =
   member this.Expr = expr
